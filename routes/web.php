d6a261706b3a555ff76355641f9ecacacc88222f
dylanholin-campus <dylan.holin@le-campus-numerique.fr>
2026-02-02 11:12:50 +0100

    test commit with remote

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function () {
+    return 'Hello World!';
+});
+
+// Route vers la page d'accueil
 Route::get('/', function () {
-    return view('welcome');
+    return 'Bienvenue sur ShopLaravel!';
 });
